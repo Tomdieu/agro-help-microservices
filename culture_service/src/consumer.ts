20688643926a625af41cfdbbe1c4ab9ed591ac30
Tomdieu <ivantomdio@gmail.com>
2023-06-04 09:06:30 +0100

    updated culture consumer

--- culture_service/src/consumer.ts
+++ culture_service/src/consumer.ts
@@ -1,4 +1,5 @@
 import amqp from "amqplib";
+import {Soil} from './models'
 var channel: amqp.Channel, connection: amqp.Connection;
 import colors from "colors";
 // connectQueue();
@@ -13,6 +14,28 @@ export async function connectQueue() {
     channel.consume("culture", (data) => {
       console.log("Recieve ".green.bold, { data });
       const receiveData = JSON.parse(data.content.toString());
+      const contentType = data.properties.contentType;
+      if(contentType=='soil_created'){
+        Soil.create({id:receiveData.data.id,type:receiveData.data.type,description:receiveData.data.description,composition:receiveData.data.composition}).then(soil=>{
+          console.log(`Soil ${soil.type} created!`)
+        })
+      }
+      if(contentType == 'soil_updated'){
+        Soil.findByPk(receiveData.data.id).then(soil=>{
+          if(receiveData.data.type!=soil.type){
+            soil.type = receiveData.data.type;
+          }
+          if(receiveData.data.description!=soil.description){
+            soil.description = receiveData.data.description;
+          }
+          if(receiveData.data.composition!=soil.composition){
+            soil.composition = receiveData.data.composition;
+          }
+          soil.save().then(updated_culture=>{
+            console.log("CUlture updated")
+          })
+        })
+      }
       console.log("Recieve Data : ", receiveData);
       console.log("Buffer Data : ", Buffer.from(data.content));
       channel.ack(data);
