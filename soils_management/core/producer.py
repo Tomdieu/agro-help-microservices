f7478e6171c44f9b7303cc139e5ad881245f00d8
Tomdieu <ivantomdio@gmail.com>
2023-06-04 09:39:45 +0100

    updated

--- soils_management/core/producer.py
+++ soils_management/core/producer.py
@@ -1,10 +1,14 @@
 import pika, json
 from django.conf import settings
-def publish(method, body: object | dict | str):
+
+
+def publish(method, body):
     try:
         # url = amqps://krimticf:rb_pr0-RABLjrHtEvKYUCa7lfQtMi8hG@jaragua.lmq.cloudamqp.com/krimticf
         credentials = pika.PlainCredentials("myuser", "mypass")
-        parameters = pika.ConnectionParameters(settings.RABBITMQ_HOST, 5672, "/", credentials)
+        parameters = pika.ConnectionParameters(
+            settings.RABBITMQ_HOST, 5672, "/", credentials
+        )
         # RabbitMq
 
         # parameters = pika.URLParameters('amqps://krimticf:rb_pr0-RABLjrHtEvKYUCa7lfQtMi8hG@jaragua.lmq.cloudamqp.com/krimticf')
@@ -12,7 +16,10 @@ def publish(method, body: object | dict | str):
         channel = connection.channel()
         properties = pika.BasicProperties(method)
         channel.basic_publish(
-            exchange="", routing_key="culture", body=json.dumps(body), properties=properties
+            exchange="",
+            routing_key="culture",
+            body=json.dumps(body),
+            properties=properties,
         )
     except Exception as e:
-        print("Could not connect to rabbitMq server",e)
+        print("Could not connect to rabbitMq server", e)
